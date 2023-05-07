c55cc0474cb0e6c4d62ca182570523321562d6e4
Deepson Shrestha <deepsonshrestha@SPMBP146.local>
2023-05-07 22:56:04 +0545

    added fact table and handled nulls

---
+++ models/marts/fact_match_detailed.sql
@@ -0,0 +1,101 @@
+with source as (
+
+    select * from {{ ref('stg_match_detailed') }}
+
+),
+
+dim_match as (
+
+    select * from {{ ref('dim_match') }}
+
+),
+
+dim_player as (
+
+    select * from {{ ref('dim_player') }}
+
+),
+
+joined_keys as (
+
+    select
+        case
+            when source.match_id is null 
+                then {{ dbt_utils.generate_surrogate_key(['null']) }}
+            when dim_match.match_key is null
+                then {{ dbt_utils.generate_surrogate_key(['\'No Match\''])}}
+            else dim_match.match_key
+        end as match_key,
+        case
+            when source.puuid is null 
+                then {{ dbt_utils.generate_surrogate_key(['null']) }}
+            when dim_player.player_key is null
+                then {{ dbt_utils.generate_surrogate_key(['\'No Match\''])}}
+            else dim_player.player_key
+        end as player_key,
+        source.team as team_key,
+        source.level as level_key,
+        source.character as character_key,
+        source.session_playtime_minutes,
+        source.afk_rounds,
+        source.friendly_fire_incoming,
+        source.friendly_fire_outgoing,
+        source.ability_cast_c,
+        source.ability_cast_e,
+        source.ability_cast_q,
+        source.ability_cast_x,
+        source.score,
+        source.kills,
+        source.deaths,
+        source.assists,
+        source.bodyshots,
+        source.headshots,
+        source.legshots,
+        source.overall_spent,
+        source.average_spent,
+        source.overall_loadout_value,
+        source.average_loadout_value,
+        source.damage_made,
+        source.damage_received
+    from source
+    left join dim_match
+        on source.match_id = dim_match.match_id
+    left join dim_player
+        on source.puuid = dim_player.valorant_puuid
+
+),
+
+final as (
+
+    select
+        match_key,
+        player_key,
+        team_key,
+        level_key,
+        character_key,
+        session_playtime_minutes,
+        afk_rounds,
+        friendly_fire_incoming,
+        friendly_fire_outgoing,
+        ability_cast_c,
+        ability_cast_e,
+        ability_cast_q,
+        ability_cast_x,
+        score,
+        kills,
+        deaths,
+        assists,
+        bodyshots,
+        headshots,
+        legshots,
+        overall_spent,
+        average_spent,
+        overall_loadout_value,
+        average_loadout_value,
+        damage_made,
+        damage_received
+    from joined_keys
+
+)
+
+select * from final
--- models/staging/stg_match_detailed.sql
+++ models/staging/stg_match_detailed.sql
@@ -22,33 +22,66 @@ stats as (
 
     select
         match_id,
-        _data->'puuid' as puuid,
-        _data->'team' as team,
-        _data->'level' as level,
-        _data->'character' as character,
-        _data->'session_playtime'->>'minutes' as session_playtime_minutes,
-        _data->'behavior'->>'afk_rounds' as afk_rounds,
-        _data->'behavior'->'friendly_fire'->>'incoming' as friendly_fire_incoming,
-        _data->'behavior'->'friendly_fire'->>'outgoing' as friendly_fire_outgoing,
-        _data->'ability_casts'->>'c_cast' as ability_cast_c,
-        _data->'ability_casts'->>'q_cast' as ability_cast_q,
-        _data->'ability_casts'->>'e_cast' as ability_cast_e,
-        _data->'ability_casts'->>'x_cast' as ability_cast_x,
-        _data->'stats'->>'score' as score,
-        _data->'stats'->>'kills' as kills,
-        _data->'stats'->>'deaths' as deaths,
-        _data->'stats'->>'assists' as assists,
-        _data->'stats'->>'bodyshots' as bodyshots,
-        _data->'stats'->>'headshots' as headshots,
-        _data->'stats'->>'legshots' as legshots,
-        _data->'economy'->'spent'->>'overall' as overall_spent,
-        _data->'economy'->'spent'->>'average' as average_spent,
-        _data->'economy'->'loadout_value'->>'overall' as overall_loadout_value,
-        _data->'economy'->'loadout_value'->>'average' as average_loadout_value,
-        _data->'damage_made' as damage_made,
-        _data->'damage_received' as damage_received
+        _data->>'puuid'::varchar as puuid,
+        _data->>'team'::varchar as team,
+        _data->>'level'::varchar as level,
+        _data->>'character'::varchar as character,
+        (_data->'session_playtime'->>'minutes')::float as session_playtime_minutes,
+        (_data->'behavior'->>'afk_rounds')::float as afk_rounds,
+        (_data->'behavior'->'friendly_fire'->>'incoming')::float as friendly_fire_incoming,
+        (_data->'behavior'->'friendly_fire'->>'outgoing')::float as friendly_fire_outgoing,
+        (_data->'ability_casts'->>'c_cast')::float as ability_cast_c,
+        (_data->'ability_casts'->>'q_cast')::float as ability_cast_q,
+        (_data->'ability_casts'->>'e_cast')::float as ability_cast_e,
+        (_data->'ability_casts'->>'x_cast')::float as ability_cast_x,
+        (_data->'stats'->>'score')::float as score,
+        (_data->'stats'->>'kills')::float as kills,
+        (_data->'stats'->>'deaths')::float as deaths,
+        (_data->'stats'->>'assists')::float as assists,
+        (_data->'stats'->>'bodyshots')::float as bodyshots,
+        (_data->'stats'->>'headshots')::float as headshots,
+        (_data->'stats'->>'legshots')::float as legshots,
+        (_data->'economy'->'spent'->>'overall')::float as overall_spent,
+        (_data->'economy'->'spent'->>'average')::float as average_spent,
+        (_data->'economy'->'loadout_value'->>'overall')::float as overall_loadout_value,
+        (_data->'economy'->'loadout_value'->>'average')::float as average_loadout_value,
+        (_data->>'damage_made')::float as damage_made,
+        (_data->>'damage_received')::float as damage_received
     from flattened
+
+),
+
+final as (
+
+    select
+        match_id,
+        puuid,
+        team,
+        level,
+        character,
+        session_playtime_minutes,
+        afk_rounds,
+        friendly_fire_incoming,
+        friendly_fire_outgoing,
+        ability_cast_c,
+        ability_cast_e,
+        ability_cast_q,
+        ability_cast_x,
+        score,
+        kills,
+        deaths,
+        assists,
+        bodyshots,
+        headshots,
+        legshots,
+        overall_spent,
+        average_spent,
+        overall_loadout_value,
+        average_loadout_value,
+        damage_made,
+        damage_received
+    from stats
 
 )
 
-select * from stats
+select * from final
